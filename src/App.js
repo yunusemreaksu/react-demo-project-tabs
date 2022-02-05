8657af392322b3bf044baff7474a2fe3baa3ccd3
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-05 14:35:06 +0300

    added fetch & async

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tabs-project'
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  
+  if(loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+  return <h2>jobs</h2>
+}
+
+export default App

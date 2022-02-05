6b30563c877863d25e636f4dc06231b639346580
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-05 14:50:42 +0300

    displaying first job

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   useEffect(() => {
     fetchJobs();
   }, []);
-  
+
   if(loading) {
     return (
       <section className='section loading'>
@@ -25,7 +25,33 @@ function App() {
       </section>
     )
   }
-  return <h2>jobs</h2>
+  const {company, dates, duties, title} = jobs[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* {btn container} */}
+        {/* {job info} */}
+        <article className='job-info'>
+          <h3> {title} </h3>
+          <h4> {company} </h4>
+          <p className='job-date'> {dates} </p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc" >
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p> {duty} </p>
+              </div>
+              
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
